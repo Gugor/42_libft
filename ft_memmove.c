bcc8625ebaee50ad079d908e8f59403224e8480b
Your Name <you@example.com>
2023-05-17 19:11:43 +0200

    memmove.c finished

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: hmontoya <hmontoya@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 18:07:56 by hmontoya          #+#    #+#             */
-/*   Updated: 2023/05/16 20:25:39 by hmontoya         ###   ########.fr       */
+/*   Updated: 2023/05/17 19:10:32 by hmontoya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,29 @@ void *ft_memmove(void *dst, const void *src, size_t len)
 {
 	size_t i;
 	unsigned char *dest;
+	unsigned char *sorc;
 
 	dest = (unsigned char *)dst;
+	sorc = (unsigned char *)src;
 	if (src == dst)
-		return ((void *)dest);
-	if (src > dst)
+	{
+		return ((void *)dst);
+	}
+	else if (sorc > dest)
 	{
 		i = 0;
-		printf("Dest: %p, src: %p",dest,src);
 		while(i < len)
 		{
-			dest[i] = (unsigned char)src + i;
+			dest[i]= *(sorc + i);
 			i++;
 		}
 	}
 	else
 	{
-		while (len-- >= 0)
+		while (len--)
 		{
-			dest[len] = (unsigned char)src + len;
+			dest[len] = *(sorc + len);
 		}
 	}
-	return ((void *)dest);
+	return ((void *)dst);
 }
