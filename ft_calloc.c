a6953e991ab10eae2125204691d0b76f11a86a9d
Your Name <you@example.com>
2023-05-24 20:33:56 +0200

    set ft_calloc.c to be done

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,35 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hmontoya <hmontoya@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/20 21:55:56 by hmontoya          #+#    #+#             */
-/*   Updated: 2023/05/24 20:32:37 by hmontoya         ###   ########.fr       */
+/*   Created: 2023/05/24 20:32:52 by hmontoya          #+#    #+#             */
+/*   Updated: 2023/05/24 20:33:14 by hmontoya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t ft_strlcat(char *dst, char *src, size_t dstsize)
-{
-	size_t src_len;
-	size_t dst_len;
-	size_t i;
-	
-    src_len = ft_strlen(src);
-	dst_len = ft_strlen(dst);
-	i = 0;
-	if(dstsize == 0)
-		return (src_len);
-	if (dstsize < dst_len)
-		return (dstsize + src_len);
-	while (i + dst_len < dstsize - 1 && src[i])
-	{
-		dst[i + dst_len] = src[i];
-		i++;
-	}
-	dst[i + dst_len] = '\0';
-	return (dst_len + src_len);
-}
+
