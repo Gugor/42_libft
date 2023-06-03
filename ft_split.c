171f2f1dc0cb75bf8a5569e7d966c984ea60e644
Your Name <you@example.com>
2023-06-03 22:28:14 +0200

    ft_split.c advancing

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: hmontoya <hmontoya@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 18:36:50 by hmontoya          #+#    #+#             */
-/*   Updated: 2023/06/03 20:28:29 by hmontoya         ###   ########.fr       */
+/*   Updated: 2023/06/03 22:27:48 by hmontoya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,24 @@ static int ft_count_cols(char *s, char c)
 	int numcols;
 
 	numcols = 0;
-	set = (char *)c;
+	set = &c;
 	s = ft_strtrim(s, set);
 	if (!s)
 	{
 		free(s);
-		return (NULL);
+		return (0);
 	}
 	while (*s)
 	{
 		if (*s == c)
+		{
+			while (*s == c)
+				s++;
 			numcols++;
-		tmp++;
+		}
+		s++;
 	}
+	numcols++;
 	return (numcols);
 }
 
@@ -40,26 +45,27 @@ static char **ft_calloc2d(char const *s, char c, int numcols)
 	int i;
 
 	i = 0;
-	if (numcols >= 1)
-		tmp = (char **)malloc(numcols * sizeof(char **));
-	else
-		return ((char **)ft_strdup(s));
-	if (!result)
+	tmp = (char **)malloc(numcols * sizeof(char **));
+	if (!tmp)
 	{
-		free(result)
+		free(tmp);
 		return (NULL);
 	}
 	numcols = 0;
+	printf("2d malloc created\n");
 	while (*(s + i))
 	{
 		if(*(s + i) == c)
 		{
 			tmp[numcols] = ft_calloc(i - 1, sizeof(s));
+			printf("Coll allocated (%p)\n", );
 			if (!tmp[numcols])
 				return (NULL);
 			numcols++;		
 		}
-	}	
+		i++;
+	}
+	printf("2d cols and rows allocated => %i\n", numcols);
 	return (tmp);
 }
 
@@ -68,18 +74,36 @@ char **ft_split(char const *s, char c)
 	char *tmp;
 	char **result;
 	int numcols;
+	int col;
+	int row;
+	int i;
 
 	if (s == NULL)
 		return (NULL);
-	numcols = ft_count_cols(tmp);
-	result = ft_calloc2d(s, c, numcols);
+	tmp = (char *)s;
+	numcols = ft_count_cols(tmp, c);
+	printf("Numcols: %i\n", numcols);
+	if (numcols == 0)
+		return (NULL);
+	result = ft_calloc2d(tmp, c, numcols);
 	if(!result)
 	{
 		free(result);
 		return (NULL);
 	}
-	while ()
+	printf("Calloc 2d done!!");
+	col = 0;
+	row = 0;
+	i = 0;
+	while (*s && col <= numcols - 1)
 	{
-		
+		result[col++][row] = *(s + row); 
+		if (*s == c)
+		{
+			row = 0;
+			s += row + 1;
+		} 
+		i++;
 	}
+	return (result);
 }
--- mains/main_split.c
+++ mains/main_split.c
@@ -6,11 +6,11 @@
 /*   By: hmontoya <hmontoya@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 18:39:35 by hmontoya          #+#    #+#             */
-/*   Updated: 2023/06/03 19:12:30 by hmontoya         ###   ########.fr       */
+/*   Updated: 2023/06/03 21:57:26 by hmontoya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../libft.h"
 
 static void ft_print2dchar(char **arrstr)
 {
@@ -28,12 +28,12 @@ int main (void)
 {
 	//Test 1
 	printf("\33[1;34mTest 1: normal input with \';\' as character separator\n");
-	char t1_s[20] = "this;is;\tmy;\nfirst; string";
+	char t1_s[50] = "this;is;\tmy;\nfirst; string";
 	char t1_c     = ';';
 
 	printf("\33[1;37mInputs:\33[1;0m\n");
-	printf("char s[] = \33[1;36m\"%s\"\33[1;0m\n",t1_s);
-	printf("char c = \33[1;36m\"%s\"\33[1;0m\n\n",t1_c);
+	printf("\33[1;32mchar\33[1;0m s[] = \33[1;36m\"%s\"\33[1;0m\n",t1_s);
+	printf("\33[1;32mchar\33[1;0m c = \33[1;36m\"%c\"\33[1;0m\n\n",t1_c);
 	printf("Result:\n");
 	ft_print2dchar(ft_split(t1_s,t1_c));
 	
