a5ccfab53ef1c5cbaad3edcfed175bebda159ea8
Your Name <you@example.com>
2023-06-06 18:05:40 +0200

    ft_split.c little advances

--- ft_split.c
+++ ft_split.c
@@ -6,11 +6,32 @@
 /*   By: hmontoya <hmontoya@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 18:36:50 by hmontoya          #+#    #+#             */
-/*   Updated: 2023/06/03 22:27:48 by hmontoya         ###   ########.fr       */
+/*   Updated: 2023/06/06 18:04:56 by hmontoya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int ft_skip_separator(const char *s, char c, int pos)
+{
+	int i;
+
+	i = pos;
+	while (*(s + i) == c)
+		i++;
+	return (i);
+}
+
+static int verify_alloc(void *alloc)
+{
+	if(!alloc)
+	{
+		free(alloc);
+		return (0);
+	}
+	else 
+		return (1);
+}
 
 static int ft_count_cols(char *s, char c)
 {
@@ -20,11 +41,8 @@ static int ft_count_cols(char *s, char c)
 	numcols = 0;
 	set = &c;
 	s = ft_strtrim(s, set);
-	if (!s)
-	{
-		free(s);
+	if (!verify_alloc(s))
 		return (0);
-	}
 	while (*s)
 	{
 		if (*s == c)
@@ -43,29 +61,29 @@ static char **ft_calloc2d(char const *s, char c, int numcols)
 {
 	char **tmp;
 	int i;
+	int last_sep_pos;
 
 	i = 0;
 	tmp = (char **)malloc(numcols * sizeof(char **));
-	if (!tmp)
-	{
-		free(tmp);
+	if (!verify_alloc(tmp))
 		return (NULL);
-	}
 	numcols = 0;
-	printf("2d malloc created\n");
+	last_sep_pos = i;
 	while (*(s + i))
 	{
 		if(*(s + i) == c)
 		{
-			tmp[numcols] = ft_calloc(i - 1, sizeof(s));
-			printf("Coll allocated (%p)\n", );
+			i += ft_skip_separator(s, c, i);
+			last_sep_pos = i - 1;
+			tmp[numcols] = ft_calloc(i - 2, sizeof(s));
 			if (!tmp[numcols])
 				return (NULL);
 			numcols++;		
 		}
 		i++;
 	}
-	printf("2d cols and rows allocated => %i\n", numcols);
+	if(i > 0)
+		tmp[++numcols] = ft_calloc(i - last_sep_pos - 1, sizeof(s));
 	return (tmp);
 }
 
@@ -86,16 +104,13 @@ char **ft_split(char const *s, char c)
 	if (numcols == 0)
 		return (NULL);
 	result = ft_calloc2d(tmp, c, numcols);
-	if(!result)
-	{
-		free(result);
+	if(!verify_alloc(result))
 		return (NULL);
-	}
 	printf("Calloc 2d done!!");
 	col = 0;
 	row = 0;
 	i = 0;
-	while (*s && col <= numcols - 1)
+	while (*s && col < numcols - 1)
 	{
 		result[col++][row] = *(s + row); 
 		if (*s == c)
