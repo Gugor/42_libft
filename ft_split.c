0145cb9b82a5449987d9fae0bb30dccb29ee2dac
Your Name <you@example.com>
2023-06-07 19:26:35 +0200

    ft_split.c memory leaks problems

--- ft_split.c
+++ ft_split.c
@@ -6,17 +6,17 @@
 /*   By: hmontoya <hmontoya@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 18:36:50 by hmontoya          #+#    #+#             */
-/*   Updated: 2023/06/06 20:34:03 by hmontoya         ###   ########.fr       */
+/*   Updated: 2023/06/07 19:25:20 by hmontoya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int ft_skip_separator(const char *s, char c, int pos)
+static size_t ft_skip_separator(const char *s, char c)
 {
-	int i;
+	size_t i;
 
-	i = pos;
+	i = 0;
 	while (*(s + i) == c)
 		i++;
 	return (i);
@@ -41,6 +41,7 @@ static int ft_count_cols(char *s, char c)
 
     set = &c;
 	numcols = 0;
+	i = 0;
 	s = ft_strtrim(s, set);
 	if (!verify_alloc(s))
 		return (0);
@@ -48,13 +49,13 @@ static int ft_count_cols(char *s, char c)
 	{
 		if (*(s + i) == c)
 		{
-			i += ft_skip_separator(s, c, i);
+			i += ft_skip_separator(s + i, c);
 			numcols++;
 		}
-		i++;
+		else
+			i++;
 	}
-	if (i > 0)
-		numcols++;
+	numcols++;
 	return (numcols);
 }
 
@@ -71,39 +72,31 @@ char **ft_split(char const *s, char c)
 	char **result;
 	size_t numcols;
     size_t currentcol;
-	size_t bow;
 	size_t eow; 
 
     tmp = (char *)s;
-    bow = 0;
-	currentcol = bow;
+	eow = 0;
+	currentcol = 0;
 	if (s == NULL)
-		return (NULL);
+	{
+		result = ft_calloc(2, sizeof(s));
+		result[0] = ft_strdup("");
+		return (result);
+	}
 	numcols = ft_count_cols(tmp, c);
-	if (numcols == 0)
-		return (NULL);
-	result = ft_calloc(numcols, sizeof(s));
-    printf("Num Cols = %zu\n", numcols);
+	result = ft_calloc(numcols + 1, sizeof(s));
 	if(!verify_alloc(result))
 		return (NULL);
-	while (*(s + bow  + eow) && currentcol < numcols - 1)
+	while (*(tmp + eow) && currentcol < numcols)
 	{
-        printf("CCol = %zu\n", numcols);
-		bow += ft_skip_separator(s, c, bow);
-		while (*(s + bow + eow) != c && *s)
-           bow ++;
-        result[currentcol++] = ft_substr(s, bow + 1, eow);
-        if(!result[currentcol])
+		tmp += ft_skip_separator(tmp, c);
+		while (*(tmp + eow) != c && *(tmp + eow))
+           eow ++;
+        result[currentcol] = ft_substr(tmp, 0, eow);
+        if(!result)
             free_all(result,currentcol);
-        currentcol++;
-	}
-    if(currentcol < 0)
-    {
-		while (*(s + bow + eow) != c && *s)
-            bow++;
-        result[currentcol++] = ft_substr(s, bow + 1, eow);
-        if(!result[currentcol])
-            free_all(result,currentcol);
+		tmp += eow;
+		eow = 0;
         currentcol++;
 	}
 	return (result);
--- mains/main_split.c
+++ mains/main_split.c
@@ -6,7 +6,7 @@
 /*   By: hmontoya <hmontoya@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 18:39:35 by hmontoya          #+#    #+#             */
-/*   Updated: 2023/06/03 21:57:26 by hmontoya         ###   ########.fr       */
+/*   Updated: 2023/06/07 19:19:59 by hmontoya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,15 @@ static void ft_print2dchar(char **arrstr)
 	i = 0;
 	while (arrstr[i])
 	{
-		while(arrstr[i])
-			printf(">>%s\n",arrstr[i]);
+		printf(">> %s\n",arrstr[i]);
+		i++;
 	}
 }
 
 int main (void)
 {
 	//Test 1
-	printf("\33[1;34mTest 1: normal input with \';\' as character separator\n");
+	printf("i\n\33[1;34mTest 1: normal input with \';\' as character separator\n");
 	char t1_s[50] = "this;is;\tmy;\nfirst; string";
 	char t1_c     = ';';
 
@@ -35,7 +35,55 @@ int main (void)
 	printf("\33[1;32mchar\33[1;0m s[] = \33[1;36m\"%s\"\33[1;0m\n",t1_s);
 	printf("\33[1;32mchar\33[1;0m c = \33[1;36m\"%c\"\33[1;0m\n\n",t1_c);
 	printf("Result:\n");
-	ft_print2dchar(ft_split(t1_s,t1_c));
+	char **t1_result = ft_split(t1_s,t1_c);
+	ft_print2dchar(t1_result);
+
+	//Test 2
+    printf("\n\33[1;34mTest 2: s is full of c\n");
+    char t2_s[50] = "     ";
+    char t2_c     = ' ';
+
+    printf("\33[1;37mInputs:\33[1;0m\n");
+    printf("\33[1;32mchar\33[1;0m s[] = \33[1;36m\"%s\"\33[1;0m\n",t2_s);
+    printf("\33[1;32mchar\33[1;0m c = \33[1;36m\"%c\"\33[1;0m\n\n",t2_c);
+    printf("Result:\n");
+	char **t2_result = ft_split(t2_s,t2_c);
+	ft_print2dchar(t2_result);
+	//Test 3
+    printf("\n\33[1;34mTest 3: one word input\n");
+    char t3_s[50] = "string";
+    char t3_c     = ':';
+
+    printf("\33[1;37mInputs:\33[1;0m\n");
+    printf("\33[1;32mchar\33[1;0m s[] = \33[1;36m\"%s\"\33[1;0m\n",t3_s);
+    printf("\33[1;32mchar\33[1;0m c = \33[1;36m\"%c\"\33[1;0m\n\n",t3_c);
+    printf("Result:\n");
+	char **t3_result = ft_split(t3_s,t3_c);
+	ft_print2dchar(t3_result);
+	
+	//Test 4
+    printf("\n\33[1;34mTest 4: one word input multiple saparator before\n");
+    char t4_s[50] = ":::::::print";
+    char t4_c     = ':';
+
+    printf("\33[1;37mInputs:\33[1;0m\n");
+    printf("\33[1;32mchar\33[1;0m s[] = \33[1;36m\"%s\"\33[1;0m\n",t4_s);
+    printf("\33[1;32mchar\33[1;0m c = \33[1;36m\"%c\"\33[1;0m\n\n",t4_c);
+    printf("Result:\n");
+	char **t4_result = ft_split(t4_s,t4_c);
+    ft_print2dchar(ft_split(t4_s,t4_c));
+
+	//Test 5
+    printf("\n\33[1;34mTest 4: phrase input space saparator\n");
+    char t5_s[100] = "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse";
+    char t5_c     = ' ';
+
+    printf("\33[1;37mInputs:\33[1;0m\n");
+    printf("\33[1;32mchar\33[1;0m s[] = \33[1;36m\"%s\"\33[1;0m\n",t5_s);
+    printf("\33[1;32mchar\33[1;0m c = \33[1;36m\"%c\"\33[1;0m\n\n",t5_c);
+    printf("Result:\n");
+    char **t5_result = ft_split(t5_s,t5_c);
+    ft_print2dchar(t5_result);
 	
 	return (0);
 }
