309f353bf32dc274aff6141d8e2ba2c29e64ab75
HugoMonVaz <hmontoyavazquez@gmail.com>
2023-06-08 12:30:17 +0200

    ft_split.c arranged to fit 25lines

--- ft_split.c
+++ ft_split.c
@@ -22,17 +22,6 @@ static size_t ft_skip_separator(const char *s, char c)
 	return (i);
 }
 
-static int verify_alloc(void *alloc)
-{
-	if(!alloc)
-	{
-		free(alloc);
-		return (0);
-	}
-	else 
-		return (1);
-}
-
 static int ft_count_cols(char *s, char c)
 {
 	char *set;
@@ -43,8 +32,11 @@ static int ft_count_cols(char *s, char c)
 	numcols = 0;
 	i = 0;
 	s = ft_strtrim(s, set);
-	if (!verify_alloc(s))
-		return (0);
+	if (!s)
+    {
+        free(s);
+        return (0);
+    }
 	while (*(s + i))
 	{
 		if (*(s + i) == c)
@@ -63,20 +55,44 @@ static void free_all(char **result, size_t cols)
 {
     while (cols > 0)
         free(result[cols--]);
+    free(result);
     result = NULL;
 }
-
+static char **set_strcols(char *s, char c, size_t maxcols)
+{
+    char **result;
+    size_t currentcol;
+    size_t eow;
+    
+    currentcol = 0;
+    eow = 0;
+	result = ft_calloc(maxcols + 1, sizeof(s));
+	if(!result)
+    {
+        free(result);
+        return (NULL);
+    }
+    while (*(s + eow) && currentcol < maxcols)
+    {
+        s += ft_skip_separator(s, c);
+        while (*(s + eow) != c && *(s + eow))
+            eow ++;
+        result[currentcol] = ft_substr(s, 0, eow);
+        if(!result)
+            free_all(result,currentcol);
+        s += eow;
+        eow = 0;
+        currentcol++;
+     }
+    return (result);
+}
 char **ft_split(char const *s, char c)
 {
 	char *tmp;
 	char **result;
 	size_t numcols;
-    size_t currentcol;
-	size_t eow; 
 
     tmp = (char *)s;
-	eow = 0;
-	currentcol = 0;
 	if (s == NULL)
 	{
 		result = ft_calloc(2, sizeof(s));
@@ -84,10 +100,8 @@ char **ft_split(char const *s, char c)
 		return (result);
 	}
 	numcols = ft_count_cols(tmp, c);
-	result = ft_calloc(numcols + 1, sizeof(s));
-	if(!verify_alloc(result))
-		return (NULL);
-	while (*(tmp + eow) && currentcol < numcols)
+    result = set_strcols(tmp, c, numcols);
+	/*while (*(tmp + eow) && currentcol < numcols)
 	{
 		tmp += ft_skip_separator(tmp, c);
 		while (*(tmp + eow) != c && *(tmp + eow))
@@ -98,6 +112,6 @@ char **ft_split(char const *s, char c)
 		tmp += eow;
 		eow = 0;
         currentcol++;
-	}
+	}*/
 	return (result);
 }
