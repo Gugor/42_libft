2ad84a7157f231a6db88323b38cb0c81eab590e8
Your Name <you@example.com>
2023-06-14 19:11:14 +0200

    ft_putnbr_fd.c norminette passed ok

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,28 +6,26 @@
 /*   By: hmontoya <hmontoya@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/27 16:01:46 by hmontoya          #+#    #+#             */
-/*   Updated: 2023/05/30 14:55:39 by hmontoya         ###   ########.fr       */
+/*   Updated: 2023/06/14 19:10:46 by hmontoya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_putnbr_fd(int n, int fd)
+void	ft_putnbr_fd(int n, int fd)
 {
-	if(n == -2147483648)
-		write(fd,"-2147483648", 11);
+	if (n == -2147483648)
+		write(fd, "-2147483648", 11);
 	else if (n < 0)
 	{
-		ft_putchar_fd('-',fd);
-		ft_putnbr_fd(-n, fd);	
+		ft_putchar_fd('-', fd);
+		ft_putnbr_fd(-n, fd);
 	}
 	else if (n >= 10)
 	{
 		ft_putnbr_fd(n / 10, fd);
-		ft_putnbr_fd(n % 10, fd);		
+		ft_putnbr_fd(n % 10, fd);
 	}
-	else 
-	{
+	else
 		ft_putchar_fd(n + '0', fd);
-	}
 }
