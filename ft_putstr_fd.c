47b1262924775b870fd0bf033c36348e33eaca98
Your Name <you@example.com>
2023-06-14 19:12:36 +0200

    ft_putstr_fd.c norminette passed ok

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,17 +6,17 @@
 /*   By: hmontoya <hmontoya@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/27 15:45:15 by hmontoya          #+#    #+#             */
-/*   Updated: 2023/05/27 16:38:31 by hmontoya         ###   ########.fr       */
+/*   Updated: 2023/06/14 19:12:06 by hmontoya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_putstr_fd(char *s, int fd)
+void	ft_putstr_fd(char *s, int fd)
 {
 	while (*s)
 	{
-		ft_putchar_fd(*s,fd);
+		ft_putchar_fd(*s, fd);
 		s++;
 	}	
 }
