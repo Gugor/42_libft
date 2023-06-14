a890b431aa66cdf0383e6e404467164878f9d7de
Your Name <you@example.com>
2023-06-14 19:31:37 +0200

    ft_striteri.c norminette passed ok

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,20 +6,20 @@
 /*   By: hmontoya <hmontoya@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 18:44:47 by hmontoya          #+#    #+#             */
-/*   Updated: 2023/05/31 19:09:03 by hmontoya         ###   ########.fr       */
+/*   Updated: 2023/06/14 19:31:12 by hmontoya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_striteri(char *s, void (*f)(unsigned int, char*))
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-	unsigned int i;
-	
+	unsigned int	i;
+
 	i = 0;
 	while (s[i])
 	{
-		f(i, &s[i]);		
+		f(i, &s[i]);
 		i++;
 	}
 }
