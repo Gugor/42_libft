6a0e2de4828fdadcebd8bf5bd76e1735357cd288
Your Name <you@example.com>
2023-06-14 19:29:42 +0200

    ft_strdu.c norminette passed ok

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,27 +6,27 @@
 /*   By: hmontoya <hmontoya@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 16:42:41 by hmontoya          #+#    #+#             */
-/*   Updated: 2023/05/31 18:47:02 by hmontoya         ###   ########.fr       */
+/*   Updated: 2023/06/14 19:29:16 by hmontoya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strdup(const char *s1)
+char	*ft_strdup(const char *s1)
 {
-	size_t i;
-	size_t len;
-	char *dup;
+	size_t	i;
+	size_t	len;
+	char	*dup;
 
-	i   = 0;
+	i = 0;
 	len = ft_strlen(s1);
 	dup = (char *)malloc(len + 1 * sizeof(char));
-	if(!dup)
+	if (!dup)
 		return (NULL);
-	while (s1[i] != '\0')
+	while (s1[i])
 	{
 		dup[i] = (char)s1[i];
-		i++;	
+		i++;
 	}
 	dup[i] = '\0';
 	return (dup);
