a7c2426e0ed0e9a01bcda3e4788edabfc2b15234
Your Name <you@example.com>
2023-06-14 19:03:04 +0200

    ft_memet.c norminette passed ok

--- ft_memset.c
+++ ft_memset.c
@@ -1,27 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                           :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hmontoya <hmontoya@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 12:55:55 by hmontoya          #+#    #+#             */
-/*   Updated: 2023/05/10 20:11:05 by hmontoya         ###   ########.fr       */
+/*   Updated: 2023/06/14 19:02:36 by hmontoya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memset(void *b, int c, size_t len)
+void	*ft_memset(void *b, int c, size_t len)
 {
-	size_t i;
-	unsigned char *uc;
+	size_t			i;
+	unsigned char	*uc;
 
-	uc = (unsigned char *)b;
+	uc = (unsigned char *) b;
 	i = 0;
 	while (i < len)
 	{
-		uc[i] = (unsigned char)c;
+		uc[i] = (unsigned char) c;
 		i++;
 	}
 	return (b);
