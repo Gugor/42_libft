49947f66f6ec3e2da6c75d85c2900195e05f7bbf
Your Name <you@example.com>
2023-06-14 19:34:43 +0200

    ft_strjoin.c norminette passed ok

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,27 +6,26 @@
 /*   By: hmontoya <hmontoya@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/27 16:51:05 by hmontoya          #+#    #+#             */
-/*   Updated: 2023/05/30 14:07:03 by hmontoya         ###   ########.fr       */
+/*   Updated: 2023/06/14 19:34:24 by hmontoya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*newstr;
-	int	len1;
-	int	len2;
+	int		len1;
+	int		len2;
 
 	len1 = ft_strlen(s1);
 	len2 = ft_strlen(s2);
-	
 	if (!s2)
 		return ((char *)s1);
-	newstr = (char *)ft_calloc(len1 + len2 + 1,sizeof(char));
+	newstr = (char *)ft_calloc(len1 + len2 + 1, sizeof(char));
 	if (!newstr)
 		return (NULL);
-	ft_strlcpy(newstr,(char *)s1,len1 + 1);
-	ft_strlcat(newstr,(char *)s2,len1 + len2 + 1);
-   	return newstr;	
+	ft_strlcpy(newstr, (char *)s1, len1 + 1);
+	ft_strlcat(newstr, (char *)s2, len1 + len2 + 1);
+	return (newstr);
 }
