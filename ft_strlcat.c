6cdb28b45c343f87d845eb5bf1279c5a60131b96
Your Name <you@example.com>
2023-06-14 19:36:14 +0200

    ft_strlcat.c norminette passed ok

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,22 +6,22 @@
 /*   By: hmontoya <hmontoya@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 21:55:56 by hmontoya          #+#    #+#             */
-/*   Updated: 2023/05/24 20:32:37 by hmontoya         ###   ########.fr       */
+/*   Updated: 2023/06/14 19:35:51 by hmontoya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t ft_strlcat(char *dst, char *src, size_t dstsize)
+size_t	ft_strlcat(char *dst, char *src, size_t dstsize)
 {
-	size_t src_len;
-	size_t dst_len;
-	size_t i;
-	
-    src_len = ft_strlen(src);
+	size_t	src_len;
+	size_t	dst_len;
+	size_t	i;
+
+	src_len = ft_strlen(src);
 	dst_len = ft_strlen(dst);
 	i = 0;
-	if(dstsize == 0)
+	if (dstsize == 0)
 		return (src_len);
 	if (dstsize < dst_len)
 		return (dstsize + src_len);
