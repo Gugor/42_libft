93e031cc68ceda223317ed74f8c61df5d839c064
Your Name <you@example.com>
2023-06-14 19:27:25 +0200

    ft_strchr.c norminette passed ok

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,28 +6,28 @@
 /*   By: hmontoya <hmontoya@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 19:51:02 by hmontoya          #+#    #+#             */
-/*   Updated: 2023/06/01 10:55:44 by hmontoya         ###   ########.fr       */
+/*   Updated: 2023/06/14 19:26:58 by hmontoya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strchr(const char *s, int c)
+char	*ft_strchr(const char *s, int c)
 {
-	unsigned char *str;
-	unsigned char ch;
-	int i;
+	unsigned char	*str;
+	unsigned char	ch;
+	int				i;
 
-	str = (unsigned char *)s;
+	str = (unsigned char *) s;
 	ch = (unsigned char) c;
 	i = 0;
-	while (str[i] != '\0')
+	while (str[i])
 	{
-		if(str[i] == ch)
+		if (str[i] == ch)
 			return ((char *)str + i);
-		i++;	
+		i++;
 	}
-	if(ch == '\0')
-		return((char *)str + i);
+	if (ch == '\0')
+		return ((char *)str + i);
 	return (NULL);
 }
