b5f41927f7019ce89cc81c05da9898bb085c8e35
Your Name <you@example.com>
2023-06-14 18:27:28 +0200

    ft_calloc.c norminette passed ok

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,20 +6,19 @@
 /*   By: hmontoya <hmontoya@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 20:32:52 by hmontoya          #+#    #+#             */
-/*   Updated: 2023/05/25 16:35:22 by hmontoya         ###   ########.fr       */
+/*   Updated: 2023/06/14 18:14:31 by hmontoya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_calloc(size_t count, size_t size)
+void	*ft_calloc(size_t count, size_t size)
 {
-	void *ptr;
+	void	*ptr;
 
 	ptr = (void *)malloc(count * size);
-	if(!ptr)
+	if (!ptr)
 		return (0);
-
-	ft_bzero(ptr,count * size);
+	ft_bzero(ptr, count * size);
 	return ((void *)ptr);
 }
