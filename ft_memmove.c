b9dc5b2cb98407212ba22af5cfe082c8723a02ff
Your Name <you@example.com>
2023-06-14 19:01:16 +0200

    ft_memmove.c norminette passe ok

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,39 +6,35 @@
 /*   By: hmontoya <hmontoya@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 18:07:56 by hmontoya          #+#    #+#             */
-/*   Updated: 2023/05/17 19:10:32 by hmontoya         ###   ########.fr       */
+/*   Updated: 2023/06/14 19:00:52 by hmontoya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memmove(void *dst, const void *src, size_t len)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	size_t i;
-	unsigned char *dest;
-	unsigned char *sorc;
+	size_t			i;
+	unsigned char	*dest;
+	unsigned char	*sorc;
 
-	dest = (unsigned char *)dst;
-	sorc = (unsigned char *)src;
+	dest = (unsigned char *) dst;
+	sorc = (unsigned char *) src;
 	if (src == dst)
-	{
-		return ((void *)dst);
-	}
+		return ((void *) dst);
 	else if (sorc > dest)
 	{
 		i = 0;
-		while(i < len)
+		while (i < len)
 		{
-			dest[i]= *(sorc + i);
+			dest[i] = *(sorc + i);
 			i++;
 		}
 	}
 	else
 	{
 		while (len--)
-		{
 			dest[len] = *(sorc + len);
-		}
 	}
-	return ((void *)dst);
+	return ((void *) dst);
 }
