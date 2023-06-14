cd1c7fa9a169ba7c088aadbb822a5a5d3471bc9d
Your Name <you@example.com>
2023-06-14 18:58:53 +0200

    ft_memcpy.c norminette passed ok

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,23 +6,23 @@
 /*   By: hmontoya <hmontoya@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 18:43:53 by hmontoya          #+#    #+#             */
-/*   Updated: 2023/06/14 17:55:06 by hmontoya         ###   ########.fr       */
+/*   Updated: 2023/06/14 18:58:17 by hmontoya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memcpy(void *dst, const void *src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t i;
-	unsigned char *tmpdst;
-	unsigned char *tmpsrc;
+	size_t			i;
+	unsigned char	*tmpdst;
+	unsigned char	*tmpsrc;
 
 	i = 0;
-	tmpdst = (unsigned char *)dst;
-	tmpsrc = (unsigned char *)src;
-	if(!dst && !src)
-		return (NULL);		
+	tmpdst = (unsigned char *) dst;
+	tmpsrc = (unsigned char *) src;
+	if (!dst && !src)
+		return (NULL);
 	while (i < n)
 	{
 		tmpdst[i] = tmpsrc[i];
