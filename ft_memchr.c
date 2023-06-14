85358457d9e007ea6eec8fd84e7a41473bb77f9b
Your Name <you@example.com>
2023-06-14 18:53:47 +0200

    ft_memchr.c norminette passed ok

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,22 +6,22 @@
 /*   By: hmontoya <hmontoya@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/13 13:49:33 by hmontoya          #+#    #+#             */
-/*   Updated: 2023/05/13 19:41:47 by hmontoya         ###   ########.fr       */
+/*   Updated: 2023/06/14 18:53:12 by hmontoya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-	unsigned char *str;
-	size_t i;
+	unsigned char	*str;
+	size_t			i;
 
 	i = 0;
 	str = (unsigned char *)s;
-	while(i < n)
+	while (i < n)
 	{
-		if(str[i] == (unsigned char)c)
+		if (str[i] == (unsigned char) c)
 			return ((void *)s + i);
 		i++;
 	}
