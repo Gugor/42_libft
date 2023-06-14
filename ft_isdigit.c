24d05faecac6dc073172004c487cc5dc16d5a2a6
Your Name <you@example.com>
2023-06-14 18:45:52 +0200

    ft_isdigit.c norminette passed ok

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,11 +6,11 @@
 /*   By: hmontoya <hmontoya@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 15:59:13 by hmontoya          #+#    #+#             */
-/*   Updated: 2023/05/03 16:14:43 by hmontoya         ###   ########.fr       */
+/*   Updated: 2023/06/14 18:45:28 by hmontoya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_isdigit(int c)
+int	ft_isdigit(int c)
 {
 	return ((c >= '0' && c <= '9'));
 }
