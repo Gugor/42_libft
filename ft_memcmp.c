aceb030da493945683617858c18ae2047defea61
Your Name <you@example.com>
2023-06-14 18:55:55 +0200

    ft_memcmp.c norminette passed ok

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,27 +6,26 @@
 /*   By: hmontoya <hmontoya@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 16:58:30 by hmontoya          #+#    #+#             */
-/*   Updated: 2023/05/16 18:07:06 by hmontoya         ###   ########.fr       */
+/*   Updated: 2023/06/14 18:55:28 by hmontoya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_memcmp(const void *s1, const void *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned char *str1;
-	unsigned char *str2;
-	size_t i;
+	unsigned char	*str1;
+	unsigned char	*str2;
+	size_t			i;
 
-	str1 = (unsigned char *)s1;
-	str2 = (unsigned char *)s2;
+	str1 = (unsigned char *) s1;
+	str2 = (unsigned char *) s2;
 	i = 0;
 	while (i < n)
 	{
-		if(str1[i] != str2[i])
-			return(str1[i] - str2[i]);
-		i++;	
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
+		i++;
 	}
 	return (0);
 }
-
